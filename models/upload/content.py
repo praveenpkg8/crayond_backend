7a85b8264f0121727dfc68bd13f7f1eeda09d9ac
praveen <praveenpkg8@gmail.com>
2019-05-22 21:43:09 +0530

    added: date, time, like_count variables and update like method

--- models/upload/content.py
+++ models/upload/content.py
@@ -1,4 +1,5 @@
 import uuid
+import datetime
 from models import database
 
 
@@ -10,11 +11,16 @@ class Content(database.Model):
     image_name = database.Column(database.String(128))
     image_path = database.Column(database.String(512))
     description = database.Column(database.String(144))
+    created_on = database.Column(database.DateTime, default=datetime.datetime.utcnow())
+    like_count = database.Column(database.Integer)
+    is_active = database.Column(database.Boolean())
 
     def __init__(self, image_name, image_path, description):
         self.image_name = image_name
         self.image_path = image_path
         self.description = description
+        self.like_count = 0
+        self.is_active = True
 
     @staticmethod
     def add_content(content: object):
@@ -27,9 +33,25 @@ class Content(database.Model):
         return feeds
 
     @staticmethod
+    def update_like(id: str):
+        feed = Content.query.get(id)
+        feed.like_count += 1
+        database.session.commit()
+        return feed.like_count
+
+    @staticmethod
+    def get_like(id: str):
+        feed = Content.query.get(id)
+        return feed.like_count
+
+    @staticmethod
     def paginate_files(page_num: int):
         files = Content.query.paginate(page=page_num ,per_page=2)
         return files
+
+
+
+
 
 
 
